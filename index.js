05d980837c641561a2ac7d4389bf1501ccdc8af8
olsen20 <olsen20@byu.edu>
2024-12-02 12:21:21 -0700

    Initial commit of Node.js application

---
+++ index.js
@@ -0,0 +1,30 @@
+// Declare variables / import classes
+const express = require('express');
+let app = express();
+let path = require('path');
+const port = 3000;
+
+// Configure server
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended : true}));
+
+// Connect to database
+// const knex = require('knex')({
+// 	client: 'pg',
+// 	connection: {
+// 		host: '',
+// 		user: 'postgres',
+// 		password: '',
+// 		database: '',
+// 		port: 5432
+// 	}
+// });
+
+// Route to display home/index page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Configure the server to start listening
+app.listen(port, () => console.log(`Website is listening on port ${port}`));
